dcec750e1a1759e0b8ac615e26e83aef60811df3
ilyailyushko <65430523+ilyailyushko@users.noreply.github.com>
2020-05-20 19:40:24 +0300

    1

---
+++ constant.py
@@ -0,0 +1,4 @@
+# Токен теграм бота
+tg_bot_token = "1154636175:AAH7YsLwwDcgCSWKz3-3dXMsmBynirQrpYk"
+# Токен pyowm
+owm_token = '3da5c421e5f19ff38733e95c7b0f14d7'
---
+++ main.py
@@ -0,0 +1,38 @@
+import constant
+from constant import tg_bot_token
+from constant import owm_token
+import telebot
+from telebot import types
+import pyowm
+
+owm = pyowm.OWM(owm_token, language='ru')  # You MUST provide a valid API key
+bot = telebot.TeleBot(tg_bot_token)
+
+# Бот здоровается с новым юзером
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "👋Привет, Напиши мне город или страну,"+
+                          "а я расскажу про погоду на данный момент!")
+
+# Бот Принимает название локации, проверяем есть ли такой город в апи OWM.
+# Если Локация найдена - показываем погоду, если не найдена,
+# - пишем 🤷‍♂️Увы, но Я не смог найти [сообщение юзера]
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    try:
+        observation = owm.weather_at_place(message.text)
+        w = observation.get_weather()
+        temp = w.get_temperature('celsius')['temp']
+        wind = w.get_wind()['speed']
+        hum = w.get_humidity()
+        answer = '👇 ' + message.text + ' 👇' + '\n\n'
+        answer += '📍____➡️ ' + w.get_detailed_status() + '\n\n'
+        answer += '🌡____➡️ ' + str(round(temp)) + ' ℃' + '\n\n'
+        answer += '🚰____➡️ ' + str(round(hum)) + ' %' + '\n\n'
+        answer += '🌬___ ➡️ ' + str(wind) + ' М.С' + '\n\n'
+
+        bot.reply_to(message, answer)
+    except:
+        bot.reply_to(message, '🤷‍♂️Увы, но Я не смог найти '+ message.text)
+
+bot.polling(none_stop=True)
